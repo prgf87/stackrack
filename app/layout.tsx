a78c84249e285feaad70e802ca4df2a3d784e62f
Pedro <prgf2011@gmail.com>
2025-02-27 14:20:21 +0000

    cleaned up app and ready to start developement#

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Stack Rack',
+  description:
+    'Welcome denizens to this particular corner of the net. Here you will find all sorts of information, pictures and early access to music and videos of Stack Rack.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
